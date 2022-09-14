1f5e902bd8311d543fbd833645a1fd6b889f56d3
Stephen Puthenpurackal <datfoosteve@gmail.com>
2022-09-14 03:11:35 -0500

    got a page up  locallly,

--- src/App.js
+++ src/App.js
@@ -7,15 +7,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.</p>
+         <p>I told you babe, this is just the start, im learning alot
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/datfoosteve"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Stephens Github
         </a>
       </header>
     </div>
